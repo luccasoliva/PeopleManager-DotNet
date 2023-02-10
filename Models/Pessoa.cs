70c3d10ea5ea0cdfc3679c0b21fd886df226b4b2
Lucas Oliveira <lucas.oliveirab0531@gmail.com>
2023-02-10 15:41:34 -0300

    Pessoa Model

---
+++ Models/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿using System.CodeDom.Compiler;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PeopleManager.Models
+{
+    public class Pessoa
+    { 
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
